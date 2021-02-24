5c2dac74b8391c74bc4d7610551c5bd5a1eee144
Alesiniya <alesya-13@mail.ru>
2021-02-25 01:08:03 +0300

    feat: add todo list

---
+++ src/Ermolaeva.Project. ToDo List/Ermolaeva.Project. ToDo List/Models/ToDoList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ermolaeva.Project._ToDo_List.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
